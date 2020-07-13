73931a3b9e018dc9efb2e9ac83d365f0feb8e3f6
unknown <aptcmsu@gmail.com>
2020-07-13 10:41:01 -0500

    Revise comments

--- euler001.py
+++ euler001.py
@@ -1,10 +1,7 @@
 # Solution in O(1) time.
-# Count numbers divisible by three, five or three and five.
-# The sum of numbers 1 to N is N*(N+1)/2.
-# Similarly, if A is the number of integers under N that are multiples of three,
-# then the sum of those numbers is (A*(A+1)/2) * 3.
-# Add the totals for numbers divisible by three and five, then subtract the total for numbers
-# divisible by fifteen to remove the overlap.
+# Count numbers divisible by three, five or three and five. The sum of numbers 1 to N is N*(N+1)/2.
+# If A is the number of integers under N that are multiples of three, then the sum of those numbers is (A*(A+1)/2) * 3.
+# Add totals for numbers divisible by three and five, then subtract the total for numbers divisible by both.
 a = (1000 - 1) // 3
 b = (1000 - 1) // 5
 c = (1000 - 1) // 15
