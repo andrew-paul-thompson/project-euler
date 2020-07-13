8a09f8795efdb96df87701d4aa62cdbd0cc88d2f
unknown <aptcmsu@gmail.com>
2020-07-13 16:06:48 -0500

    Add solution to Problem 9

---
+++ euler009.py
@@ -0,0 +1,8 @@
+n = 1000
+m = -1
+for a in range(3, (n // 3) + 1):
+    b = (n ** 2 - 2 * a * n) // (2 * n - 2 * a)
+    c = n - b - a
+    if a ** 2 + b ** 2 == c ** 2 and a * b * c > m:
+        m = a * b * c
+print(m)
