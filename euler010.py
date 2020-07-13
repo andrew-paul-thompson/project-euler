e7241bd3e7e13915933fbc4e616f3238cbda2bde
unknown <aptcmsu@gmail.com>
2020-07-13 16:16:43 -0500

    Add solution to Problem 10

---
+++ euler010.py
@@ -0,0 +1,13 @@
+def sieve(n=120000):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while p * p <= n:
+        if prime[p]:
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+    return [p for p in range(2, n + 1) if prime[p]]
+
+
+primes = sieve(2000000)
+print(sum([p for p in primes if p < 2000000]))
